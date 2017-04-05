3ff11fa891c8d4aa06feafce3645e08b68412be8
OlaJFrick <info@culturehero.org>
2017-04-05 15:13:01 +0200

    Added Navbar with links (included font-awesome in libs)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,9 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'my-app',
 	template: `
+	<navigation></navigation>
+	<router-outlet></router-outlet>
 
-		<nav>
-			<a routerLink="/front-page"
-				routerLinkActive="active">Framsida</a>
-
-			<a routerLink="/sell-page"
-				routerLinkActive="active">Sälj</a>
-
-			<a routerLink="/buy-page"
-				routerLinkActive="active">Köp</a>
-
-			<a routerLink="/about-us"
-				routerLinkActive="active">Om oss</a>
-
-			<a routerLink="/contact-us"
-				routerLinkActive="active">Kontakta oss</a>
-		</nav>
-
-		<router-outlet></router-outlet>
 	`,
 	styleUrls: []
 })
