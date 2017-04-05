2a0306672a887bde53c06f30ec066ef2f8552e4c
OlaJFrick <info@culturehero.org>
2017-04-05 16:48:56 +0200

    fixed nav-collapse mobile issues

--- src/app/navigation/navigation.component.js
+++ src/app/navigation/navigation.component.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var NavigationComponent = (function () {
     function NavigationComponent() {
-        this.isNavbarCollapsed = false;
+        this.isNavbarCollapsed = true;
     }
     return NavigationComponent;
 }());
