160b0de73b7417e42e121cfba1cbc6d38df6fd72
OlaJFrick <info@culturehero.org>
2017-04-05 16:31:28 +0200

    Revert "Remade the navbar without using jQuery"
    This reverts commit 141b1497490f428aa0edd388a5977425a5203600.

--- src/app/navigation/navigation.component.js
+++ src/app/navigation/navigation.component.js
@@ -8,7 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var NavigationComponent = (function () {
     function NavigationComponent() {
-        this.isNavbarCollapsed = false;
     }
     return NavigationComponent;
 }());
