07bf5ce5a86ab33329d8c3ee6cd01ee279fc232a
OlaJFrick <info@culturehero.org>
2017-04-19 16:24:33 +0200

    Bugfixed phonepipe that caused problems with starting up Angular

--- src/app/pipes/phone-format.pipe.js
+++ src/app/pipes/phone-format.pipe.js
@@ -5,17 +5,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("angular2/core");
+var core_1 = require("@angular/core");
 var PhonePipe = (function () {
     function PhonePipe() {
     }
     PhonePipe.prototype.transform = function (val, args) {
         val = val.charAt(0) != 0 ? '0' + val : '' + val;
         var newStr = '';
-        for (i = 0; i < (Math.floor(val.length / 2) - 1); i++) {
+        for (var i = 0; i < (Math.floor(val.length / 2) - 1); i++) {
             newStr = newStr + val.substr(i * 2, 2) + '-';
         }
-        return newStr + val.substr(i * 2);
+        // return newStr + val.substr(let i*2);
     };
     return PhonePipe;
 }());
