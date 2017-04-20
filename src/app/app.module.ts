8d139572e3bd6f352a66cea85f96b31db68ce84f
Hampus Johansson <hampusjohansson87@gmail.com>
2017-04-20 09:44:53 +0200

    Added spacing to curency again.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MemService } from './services/mem.service';
 
 import { BreakRowPipe } from './pipes/break-row.pipe';
 import { OrderByPipe } from './pipes/order-by.pipe';
+import { SpacingCurrencyPipe } from './pipes/spacing-currency.pipe';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -58,7 +59,8 @@ import { AppRoutingModule } from './app-routing.module';
 		NgbdModalBasic,
 		BrokerMasterComponent,
 		BreakRowPipe,
-		OrderByPipe
+		OrderByPipe,
+		SpacingCurrencyPipe
 	],
   	bootstrap: [
   		AppComponent
