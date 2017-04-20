bc67b60ec450ac1401f86f1ec74c249c104fe018
Hampus Johansson <hampusjohansson87@gmail.com>
2017-04-20 14:57:47 +0200

    Added icon with modal function to about us page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MemService } from '../services/mem.service';
 
 @Component({
 	selector: 'about-us',
 	templateUrl: './about-us.html',
-	styleUrls: ['./about-us.css']
+	styleUrls: ['./about-us.css'],
+	providers: [MemService]
 })
 
 export class AboutUsComponent  {
+
+	constructor(
+        private memService: MemService
+    ){
+        this.globalMem = this.memService.global();
+    }
+
+    closeNav(){
+        this.isNavbarCollapsed = true;
+    }
     
 }
